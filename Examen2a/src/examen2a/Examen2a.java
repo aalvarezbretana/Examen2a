56cf43ef303277f870b9346f3ac069f2a842cb75
aalvarezbretana <aalvarezbretana@danielcastelao.org>
2020-03-11 13:57:41 +0100

    Rojo master

---
+++ Examen2a/src/examen2a/Examen2a.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen2a;
+
+/**
+ *
+ * @author aalvarezbretana
+ */
+public class Examen2a {
+
+    /**
+     * @param args the command line arguments
+     * sout con rojo + commit en la master
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Rojo");
+    }
+    
+}
